0d74e481e3008b37696b79eebe99f5dfec667951
Carlos Lozano <carlos.elp94@gmail.com>
2019-01-13 22:29:13 -0400

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Carlos Lozano on 1/13/19.
+//  Copyright © 2019 Carlos Lozano. All rights reserved.
+//
+
+import Foundation
+
+
+class Item{
+  var title : String = ""
+  var done : Bool = false
+}
